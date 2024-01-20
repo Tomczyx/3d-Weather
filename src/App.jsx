39b9d0e30d5a19131af54d65755e3688e13918ef
Jacob <tomczykjakub457@gmail.com>
2024-01-20 16:09:03 +0100

    Added spline resources

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline';
+import './index.css'
+
+export default function App() {
+  return (
+    <>
+    <Spline scene="https://prod.spline.design/JO5ARl7Bt7uZ9ae1/scene.splinecode" />
+    <p>2024 &copy;</p>
+    </>
+  );
+}
